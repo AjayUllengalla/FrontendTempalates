d925c05990a40664923d633aa04c06d9fc28cbf5
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-11-10 17:09:50 +0530

    added increment every number in the array by 1, but only if the number is greater than 10 using map ,program

--- JS/Tasks.js
+++ JS/Tasks.js
@@ -22,3 +22,37 @@ let value = false;
 console.log(typeof str);
 console.log(typeof n);
 console.log(typeof value);
+
+//combining two arrays
+
+const arrays = [[1, 2], [3, 4], [5, 6]];
+let newArray = arrays.reduce((acc,curr) => {
+    return acc.concat(curr);
+})
+console.log(newArray)
+
+let newArray1 = arrays.flat();
+console.log(newArray1)
+
+// remove any negative numbers and square the remaining numbers. How would you do this using map()
+
+const numbers = [1, -2, 3, -4, 5,6];
+let newArrays = numbers.map((ele) => {
+    if(ele > 0){
+        return ele*ele;
+    }
+}).filter((value) =>{
+    return value > 0;
+})
+console.log(newArrays)
+
+//You want to increment every number in the array by 1, but only if the number is greater than 10. How would you do this using map()?
+const numbers1 = [5, 12, 8, 15, 3, 20];
+let result = numbers.map((value) => {
+    if(value < 10) {
+        return value;
+    } else {
+        return value +1
+    }
+})
+console.log(result)
