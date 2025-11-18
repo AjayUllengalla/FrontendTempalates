e427c4976a7aa13a5574c92e1ce3ba8ca0d93cf9
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-11-18 12:07:42 +0530

    Added Event Bubbling and Event Capturing

--- JS/events.js
+++ JS/events.js
@@ -13,3 +13,27 @@ btn1.addEventListener("click",show1)
 function show1(){
     alert('Hello From Event Listener')
 }
+
+function show2() {
+    alert('Mouse Event when leave cursor from button')
+}
+
+let btn4 =document.getElementById("btn4");
+btn4.addEventListener("click",()=>{
+    event.stopPropagation()
+    alert("Hello from Button Event Bubling");
+})
+
+let div = document.getElementById("div")
+div.addEventListener("click",()=> {
+    alert("Hlelo From Div Event bubbling")  // to stop this we event.stopPropagation
+})
+
+function showEventCapturing(){
+    alert('hello div from Event Capturing')
+}
+
+function eventcpaturing() {
+    event.stopPropagation();
+    alert("Button from Event Capturing")
+}
