edd41b9bb40114155fa7af16d707bdb0ce3196eb
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-11-19 18:42:13 +0530

    Uploaded Simple To-do-list with only input take

---
+++ To-Do-List/index.js
@@ -0,0 +1,18 @@
+ let text;
+    let btn = document.createElement("button")
+    btn.textContent = "Add"
+    document.getElementById("to-add").appendChild(btn)
+    btn.classList.add("btn")
+let listItem;
+    btn.addEventListener("click", () => {
+        // console.log("hello")
+         listItem = document.createElement("li")
+        text = document.getElementById("input").value
+        listItem.textContent = text 
+        document.getElementById("list").appendChild(listItem)
+         listItem.classList.add("li-items")
+          let btn2 = document.createElement("button")
+    btn2.textContent="Delete"
+    document.getElementsByClassName("li").appendChild(btn2)
+    })
+   
