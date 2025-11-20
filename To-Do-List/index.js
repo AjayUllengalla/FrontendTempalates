ce73dc0b85f550edbc38013940af2427a3518c09
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-11-20 20:00:34 +0530

    Updated

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -1,17 +1,38 @@
- let text;
-    let btn = document.createElement("button")
-    btn.textContent = "Add"
-    document.getElementById("to-add").appendChild(btn)
-    btn.classList.add("btn")
+let text;
+let empty = '';
+let btn = document.createElement("button")
+btn.textContent = "Add"
+document.getElementById("to-add").appendChild(btn)
+btn.classList.add("btn")
 let listItem;
-    btn.addEventListener("click", () => {
-        // console.log("hello")
-         listItem = document.createElement("li")
-        text = document.getElementById("input").value
-        listItem.textContent = text 
-        document.getElementById("list").appendChild(listItem)
-         listItem.classList.add("li-items")
-    })
+btn.addEventListener("click", () => {
+    // console.log("hello")
+    listItem = document.createElement("li")
+    text = document.getElementById("input").value
+    listItem.textContent = text
+    // document.getElementById("list-div").appendChild(listItem)
+    document.getElementById("list").appendChild(listItem)
+
+    listItem.classList.add("li-items")
+    
     let btn2 = document.createElement("button")
-    btn2.textContent="Delete"
-    document.getElementsByClassName("li").appendChild(btn2)
+    btn2.textContent = "Delete"
+    btn2.classList.add("del-button")
+    // document.getElementById("list-div").appendChild(btn2)
+
+    btn2.addEventListener("click", () => {
+        listItem.remove();
+    });
+
+    listItem.appendChild(btn2)
+    document.getElementById("list").appendChild(listItem)
+
+     document.getElementById("input").value = "";
+});
+// text=document.getElementById("input").value=" ";
+// listItem.textContent=text;
+// document.getElementById("list").appendChild(listItem)
+
+
+
+
