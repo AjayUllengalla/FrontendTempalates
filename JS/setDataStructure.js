4ff1c3cf094fba6bf216a0f32474f6068e91da16
Ajay Ullengalla <ullengallaajay28@gmail.com>
2025-11-29 22:00:55 +0530

    Updated

---
+++ JS/setDataStructure.js
@@ -0,0 +1,14 @@
+//Set DataStructure
+
+let setData = new Set();
+
+setData.add(2)
+setData.add(5)
+setData.add(16)
+console.log(setData)
+let arr = [1,2,3,4,5,6];
+setData.add(arr)
+console.log(setData)
+setData.add(2)
+setData.add(5)
+console.log(setData)
